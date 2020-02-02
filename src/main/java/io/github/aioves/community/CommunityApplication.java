b47d8e130360a229baa65a0ed9e102c1ebe002c3
aioves <aioves@foxmail.com>
2020-02-02 16:28:12 +0800

    初始化项目

---
+++ src/main/java/io/github/aioves/community/CommunityApplication.java
@@ -0,0 +1,21 @@
+package io.github.aioves.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Title:      项目启动类
+ * @Remarks:
+ * @Author: <a href="mailto:aioves@foxmail.com>aioves</a>
+ * @Version: 1.0.0
+ * @Date: 2020/2/2 15:25
+ *
+ */
+@SpringBootApplication
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/aioves/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.aioves.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
